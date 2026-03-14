494d23a92beadc90aa150d888767d0300ee9e171
Bhanu <bhanudurgaprasad99@gmail.com>
2026-03-14 23:41:15 +0530

    Added user login

---
+++ server/src/main/java/com/wtd/dataLoader/Services/impl/UserDetailsServiceImpl.java
@@ -0,0 +1,68 @@
+package com.wtd.dataLoader.Services.impl;
+
+import com.wtd.dataLoader.Dto.UserRepository;
+import com.wtd.dataLoader.Entity.User;
+import com.wtd.dataLoader.Services.UserDetailsService;
+import com.wtd.dataLoader.Utility.EncryptionUtility;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean validateUser(String username, String password) {
+
+        log.debug("Validating user with username: {}", username);
+        String encryptedPassword = EncryptionUtility.encrypt(password);
+        User user = userRepository.getUserByUsername(username);
+        return encryptedPassword.equals(user.getPassword());
+    }
+
+    public User getUserDetailsByUsername(String username) {
+        log.debug("Fetching user details for username: {}", username);
+        return userRepository.getUserByUsername(username);
+    }
+
+    public User getUserDetailsByID(String id) {
+        log.debug("Fetching user details for email: {}", id);
+        return userRepository.getUserById(id);
+    }
+
+    public String getUserIdByUsername(String username) {
+        log.debug("Fetching user ID for username: {}", username);
+        return userRepository.getUserIdByUsername(username);
+    }
+
+    public User saveUserDetails(User user) {
+        log.debug("Saving user details for username: {}", user.getUsername());
+        String encryptedPassword = EncryptionUtility.encrypt(user.getPassword());
+        User userBuilder = User.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .password(encryptedPassword)
+                .phoneNumber(user.getPhoneNumber())
+                .defaultShippingAddress(user.getDefaultShippingAddress())
+                .city(user.getCity())
+                .state(user.getState())
+                .zipCode(user.getZipCode())
+                .country(user.getCountry())
+                .build();
+       return userRepository.save(userBuilder);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.getUserByUsername(username);
+    }
+
+    public User getUserById(String id){
+        log.debug("Fetching user details for ID: {}", id);
+        return userRepository.getUserById(id);
+    }
+
+}
